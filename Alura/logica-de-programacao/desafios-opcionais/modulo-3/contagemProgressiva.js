f6a31392541d4c1a2203b08b44d5aeb36138b10d
JoaoVictork <joaozin10k@gmail.com>
2025-03-04 10:54:26 -0300

    desafios propostos no módulo 3

---
+++ Alura/logica-de-programacao/desafios-opcionais/modulo-3/contagemProgressiva.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um número final para a contagem"))
+let contador = 0
+alert("Contagem progressiva em segundos")
+while(contador <= numero){
+    alert(`${contador} segundos`)
+    contador++ 
+}
---
+++ Alura/logica-de-programacao/desafios-opcionais/modulo-3/contagemRegressiva.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Digite um número para a contagem"))
+alert("Contagem regressiva em segundos")
+while(numero >= 0){
+    alert(`${numero} segundos`)
+    numero-- 
+}
