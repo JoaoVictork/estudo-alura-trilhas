037928618d7ea2b66cdfcaa5c2b05dd760469c4e
JoaoVictork <joaozin10k@gmail.com>
2025-03-11 12:24:27 -0300

    desafios propostos no módulo 4

---
+++ Alura/logica-de-programacao-2/desafios-opcionais/modulo-4/listaDeDesafiosSobreListas.js
@@ -0,0 +1,18 @@
+// desafio 1
+let listagenerica = [];
+
+// desafio 2
+let linguagensDeProgramacao = ['Javascript','C','C++','Kotlin','Python'];
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+// desafio 3
+let listaDeCoisasQueEuGosto = ['Java','SpringBoot','MySQL'];
+console.log(linguagensDeProgramacao[0]);
+
+// desafio 4
+console.log(linguagensDeProgramacao[1]);
+
+// desafio 5
+console.log(linguagensDeProgramacao[2]);
