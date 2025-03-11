4e41b54e9d629a51c2a8fe761629487e0b3b52bd
JoaoVictork <joaozin10k@gmail.com>
2025-03-11 09:56:30 -0300

    alterações que esqueci de atualizar no repositório

--- Alura/logica-de-programacao-2/js-curso-2-aula1/app.js
+++ Alura/logica-de-programacao-2/js-curso-2-aula1/app.js
@@ -1,17 +1,43 @@
+//let numeroSecreto = 4;
 let numeroSecreto = gerarNumeroSecreto();
+let tentativas = 1;
 
 escreverTexto('h1', 'Hora do Desafio');
-escreverTexto('p', 'vamos começar');
+escreverTexto('p', 'digite um número de 1 a 10');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você acertou com ${tentativas} ${palavraTentativas}`;
+
+    if(chute == numeroSecreto){
+        escreverTexto('h1','Você acertou');
+        escreverTexto('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
+    else{
+        if(chute > numeroSecreto){
+            escreverTexto('p','o número é menor, tente novamente.');
+            limparCampo();  
+        }
+        else{
+            escreverTexto('p','o número é maior, tente novamente.');
+            limparCampo();
+        }
+        tentativas++
+    }
+}
 
 function escreverTexto(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-function verificarChute(){
-    console.log("o botao foi clicado!");
-}
-
 function gerarNumeroSecreto(){
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
 }
