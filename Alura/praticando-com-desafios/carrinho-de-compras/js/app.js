5330fbfa5cd5dbf5dfd8f2cc7f87a0a83f39cf79
JoaoVictork <joaozin10k@gmail.com>
2025-03-25 12:23:23 -0300

    funcionalidade limpar e ajustes no carrinho

--- Alura/praticando-com-desafios/carrinho-de-compras/js/app.js
+++ Alura/praticando-com-desafios/carrinho-de-compras/js/app.js
@@ -1,16 +1,28 @@
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = 'R$ 0';
+
 
 function adicionar(){
     let item = document.getElementById('produto').value;
     let nomeProduto = item.split('-')[0];
     let valorProduto = item.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
-    alert(nomeProduto);
-    alert(valorProduto);
-    alert(quantidade);
     let preco = valorProduto * quantidade;
-    alert(preco);
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+    <span class= "texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+    
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar(){
-
+    let totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
 }
