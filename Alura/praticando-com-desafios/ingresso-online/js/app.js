c8bed2a3aabc9c229be49f36692dfcebb666af2c
Fulano de TalJoaoVictork <joaozin10k@gmail.com>
2025-03-31 11:53:42 -0300

    funcionalidade de comprar ingresso para outros tipos, projeto finalizado

--- Alura/praticando-com-desafios/ingresso-online/js/app.js
+++ Alura/praticando-com-desafios/ingresso-online/js/app.js
@@ -3,7 +3,13 @@ function comprar(){
     let qtd = parseInt(document.getElementById('qtd').value);
 
    if (tipo.value == 'pista'){
-        comprarPista(qtd);
+    comprarPista(qtd);
+   }
+   else if(tipo.value == 'superior'){
+    comprarSuperior(qtd);
+   }
+   else if(tipo.value == 'inferior'){
+    comprarInferior(qtd);
    }
 }
 
@@ -14,6 +20,28 @@ function comprarPista(qtd){
     } else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarSuperior(qtd){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if(qtd > qtdSuperior){
+        alert('Quantidade indisponível para esse tipo');
+    } else {
+        qtdSuperior -= qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarInferior(qtd){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if(qtd > qtdInferior){
+        alert('Quantidade indisponível para esse tipo');
+    } else {
+        qtdInferior -= qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso');
     }
 }
