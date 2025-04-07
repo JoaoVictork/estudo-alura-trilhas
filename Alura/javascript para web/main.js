ca54269dd44c1c3e30d8a967518b7a209ec63578
Fulano de TalJoaoVictork <joaozin10k@gmail.com>
2025-04-06 23:27:13 -0300

    teclas tendo funções proprias

---
+++ Alura/javascript para web/main.js
@@ -0,0 +1,47 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla__pom').onclick = tocaSomPom;
+
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla__tim').onclick = tocaSomTim;
+
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla__puff').onclick = tocaSomPuff;
+
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla__splash').onclick = tocaSomSplash;
+
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla__toim').onclick = tocaSomToim;
+
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla__psh').onclick = tocaSomPsh;
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla__tic').onclick = tocaSomTic;
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla__tom').onclick = tocaSomTom;
